fece9fd3a77d011b6d36c9a8e242e80600c11d79
Gabrielzauso <gapaes4@gmail.com>
2022-08-04 23:20:08 -0300

    Subindo projeto

---
+++ script.js
@@ -0,0 +1,61 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if (APIResponse.status === 200){
+    const data = await APIResponse.json();
+    return data;
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
+    const data = await fetchPokemon(pokemon);
+
+    if(data){ // se data trazer algum valor, executa oque ta em baixo... se não (undefined ou null), not found!
+    pokemonImage.style.display = 'block'
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    searchPokemon = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+    input.value = '';
+    } else{
+        pokemonName.innerHTML = 'Not found :c';
+        pokemonNumber.innerHTML = '';
+        pokemonImage.style.display = 'none'
+    }
+    
+}
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+
+})
+
+
+buttonNext.addEventListener('click', ()=> {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon)
+})
+
+buttonPrev.addEventListener('click', ()=> {
+    if(searchPokemon > 1){
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon)
+    }
+
+})
+renderPokemon(searchPokemon);
